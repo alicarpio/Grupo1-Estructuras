9d5bac4af855996d18c0612ffbdc89e865b29e22
wgcotera <wgcotera@espol.edu.ec>
2021-11-30 04:48:27 -0500

    Mejoras en vista para cambio de letra

--- src/main/java/proyecto/grupo1/sopaletras/FX/Util.java
+++ src/main/java/proyecto/grupo1/sopaletras/FX/Util.java
@@ -1,9 +1,12 @@
 package proyecto.grupo1.sopaletras.FX;
 
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.*;
 
 public class Util {
@@ -31,17 +34,30 @@ public class Util {
 
     public static String input(String prompt) {
         Stage popup = new Stage();
-        VBox root = new VBox(10);
+        popup.setResizable(false);
+        VBox root = new VBox(20);
+        root.setAlignment(Pos.CENTER);
 
         Label lbl = new Label(prompt);
+        lbl.setFont(Font.font("System", FontWeight.NORMAL, 18));
+        
         TextField txtField = new TextField();
+        txtField.setPrefHeight(50);
+        txtField.setPrefWidth(100);
+        txtField.setAlignment(Pos.CENTER);
+        txtField.setFont(Font.font("System", FontWeight.NORMAL, 18));
+        
         HBox hbox = new HBox(20, lbl, txtField);
+        hbox.setAlignment(Pos.CENTER);
 
-        Button btn = new Button("Ok");
+        Button btn = new Button("Cambiar");
+        btn.setPrefHeight(50);
+        btn.setPrefWidth(200);
+        btn.setFont(Font.font("System", FontWeight.NORMAL, 24));
         btn.setOnAction(e -> popup.close());
 
         root.getChildren().addAll(hbox, btn);
-        Scene scene = new Scene(root, 200, 200);
+        Scene scene = new Scene(root, 300, 200);
 
         popup.setScene(scene);
         popup.setTitle("Input");
